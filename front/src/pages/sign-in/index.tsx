097f4a8d6ad194324143e1f3228f615619e33d9c
Tristan LEROY <tristan.leroy59@gmail.com>
2024-03-20 10:00:40 +0100

    bug yml

--- front/src/pages/sign-in/index.tsx
+++ front/src/pages/sign-in/index.tsx
@@ -1,11 +1,11 @@
-import { Form } from "@/components/form";
+import { Form } from '@/components/form'
 
 export function SignIn() {
-    return(
-        <main className="flex">
-        <section className="bg-[#12002b] h-[calc(100vh-12rem)] flex justify-center flex-1">
-            <Form />
-        </section>
-        </main>
-    )
+  return (
+    <main className='flex'>
+      <section className='bg-[#12002b] h-[calc(100vh-10rem)] lg:h-[calc(100vh-12rem)] flex justify-center flex-1'>
+        <Form />
+      </section>
+    </main>
+  )
 }
